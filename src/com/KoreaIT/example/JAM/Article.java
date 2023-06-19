b9919851a9763f79c39412658a0c6c6a3f98d1b2
TaekMinJeong <yuiointer7@daum.net>
2023-06-19 18:29:20 +0900

    기본적인 게시물 작성, 리스트 구현

--- src/com/KoreaIT/example/JAM/Article.java
+++ src/com/KoreaIT/example/JAM/Article.java
@@ -1,5 +1,18 @@
 package com.KoreaIT.example.JAM;
 
-public class Article {
-
+public class Article extends Object {
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+	int id;
+	String title;
+	String body;
+	
+	@Override
+//	toString() 을 사용하기 위해서는 Object 를 상속받아야함
+	public String toString() {
+		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
+	}
 }
