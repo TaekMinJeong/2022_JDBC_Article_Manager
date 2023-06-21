038545635548ab6575d092cb627f88ad776dc4b4
TaekMinJeong <yuiointer7@daum.net>
2023-06-21 23:09:48 +0900

    게시물 리스팅 시 데이터를 DB로부터 가져오기

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -3,6 +3,7 @@ package com.KoreaIT.example.JAM;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +12,6 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] arge) {
 		Scanner sc = new Scanner(System.in);
-		
-		List<Article> articles = new ArrayList<>();
 		
 		int lastArticleId = 0;
 		
@@ -28,9 +27,6 @@ public class Main {
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				
-				Article article = new Article(id, title, body);
-				articles.add(article);
 				
 				
 				
@@ -87,9 +83,78 @@ public class Main {
 				
 //				System.out.println(article);
 				
-				System.out.printf("%d번 글이 생성되었습니다.\n", article.id);
+//				System.out.printf("%d번 글이 생성되었습니다.\n", article.id);
 			}else if(cmd.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
+				
+				
+				
+				Connection con = null;
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+				
+				List<Article> articles = new ArrayList<>();
+				
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					con = DriverManager.getConnection(
+							"jdbc:mysql://localhost:3306/jdbc_article_manager?serverTimezone=UTC",	//	DB URL
+							"root", "");	//	USER_NAME 과 PASSWORD
+					System.out.println("Success");
+					
+					String sql = "SELECT * "
+							+ "FROM article "
+							+ "ORDER BY id DESC;";
+					
+					pstmt = con.prepareStatement(sql);
+					rs = pstmt.executeQuery();
+					
+					while(rs.next()) {
+						int id = rs.getInt("id");
+						String regDate = rs.getString("regDate");
+						String updateDate = rs.getString("updateDate");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+						
+						Article article = new Article(id, regDate, updateDate, title, body);
+						articles.add(article);
+					}
+					
+				}catch(SQLException ex) {
+					System.out.println("SQLException" + ex);
+					ex.printStackTrace();
+				}
+				catch(Exception ex) {
+					System.out.println("Exception" + ex);
+					ex.printStackTrace();
+				}finally {
+					try {
+						if(rs != null && !rs.isClosed()) {
+							rs.close();
+						}
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if(pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if(con != null && !con.isClosed()) {
+							con.close();
+						}
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				
+				
+				
+				
 				
 				if(articles.size() == 0) {
 					System.out.println("게시물이 없습니다");
