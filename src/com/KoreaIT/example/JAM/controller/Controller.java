08881bb1e43d42eced0ce9bd893d943299b5d065
TaekMinJeong <yuiointer7@daum.net>
2023-06-27 19:19:23 +0900

    MemberController, ArticleController 도입

---
+++ src/com/KoreaIT/example/JAM/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.KoreaIT.example.JAM.controller;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class Controller {
+	public Scanner sc;
+	
+	public Controller(Scanner sc) {
+		this.sc = sc;
+	}
+}
